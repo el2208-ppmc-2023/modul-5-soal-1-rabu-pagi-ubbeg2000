5573e2443a359bc6f35aba7e7b1a178102f7b0c1
Elkhan <74042126+ubbeg2000@users.noreply.github.com>
2023-03-14 13:58:50 +0700

    Update main.c

--- main.c
+++ main.c
@@ -8,23 +8,61 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main()
-{
+// fungsi rekursif untuk melakukan perkalian digit
+int perkalianJodoh(int num, int iterasi) {
+
+    int n = num;
+    int hasilPerkalian = 1;
+
+    // perkalian
+    while (n > 0) {
+        if (n % 10 != 0) {
+            hasilPerkalian *= (n % 10);
+        }
+        n = floor(n / 10);
+    }
+
+    // output hasil iterasi
+    printf("Iterasi #%d: %d\n", iterasi, hasilPerkalian);
+
+    if (hasilPerkalian < 10) {
+        // base case
+        return hasilPerkalian;
+    } else {
+        // recursion
+        return perkalianJodoh(hasilPerkalian, iterasi + 1);
+    }
+}
+
+int main() {
+
+    int jurusan1, jurusan2, angkatan1, angkatan2, angkaJodoh;
+
     printf("#1\n");
     printf("Masukkan kode jurusan: ");
+    scanf("%d", &jurusan1);
     printf("Masukkan angkatan: ");
+    scanf("%d", &angkatan1);
 
     printf("\n#2\n");
     printf("Masukkan kode jurusan: ");
+    scanf("%d", &jurusan2);
     printf("Masukkan angkatan: ");
+    scanf("%d", &angkatan2);
 
     printf("\n");
 
-    // printf("Iterasi #%d: %d\n", iterasi, hasilPerkalian);
+    // penjumlahan untuk menggabungkan semua angka
+    angkaJodoh = perkalianJodoh(jurusan1 * 10000000 + angkatan1 * 100000 + jurusan2 * 100 + angkatan2, 1);
 
-    // printf("\nNay!");
-    // printf("\nYay!");
+    // final output
+    if (angkaJodoh % 2 == 0) {
+        printf("\nNay!");
+    } else {
+        printf("\nYay!");
+    }
 
     return 0;
 }
